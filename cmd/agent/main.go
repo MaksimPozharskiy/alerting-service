88d6f8ee1d9347091b14e56d106a562015b21811
Maksim Pozharskiy <pozharskii@bifit.com>
2024-12-30 14:57:24 +0300

    feat: add agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"alerting-service/internal/agent"
+	"net/http"
+)
+
+func main() {
+	client := &http.Client{
+		Transport: &http.Transport{},
+	}
+
+	agent.RuntimeAgent(client)
+}
---
+++ internal/agent/agent.go
@@ -0,0 +1,121 @@
+package agent
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+const pollInterval = 2
+const reportInterval = 10
+
+var pollCount int
+
+type stats map[string]float64
+
+func RuntimeAgent(client *http.Client) {
+	memStat := &runtime.MemStats{}
+
+	runtime.ReadMemStats(memStat)
+
+	var stats = make(map[string]float64)
+
+	go func() {
+		for {
+			time.Sleep(pollInterval * time.Second)
+			pollCount++
+			getMemStatData(memStat, stats)
+
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(reportInterval * time.Second)
+			stats["RandomValue"] = rand.Float64()
+			sendMetrics(client, stats)
+		}
+	}()
+
+	select {}
+}
+
+func getMemStatData(memStat *runtime.MemStats, stats stats) {
+	stats["Alloc"] = float64(memStat.Alloc)
+	stats["BuckHashSys"] = float64(memStat.BuckHashSys)
+	stats["Frees"] = float64(memStat.Frees)
+	stats["GCCPUFraction"] = float64(memStat.GCCPUFraction)
+	stats["GCSys"] = float64(memStat.GCSys)
+	stats["HeapAlloc"] = float64(memStat.HeapAlloc)
+	stats["HeapIdle"] = float64(memStat.HeapIdle)
+	stats["HeapInuse"] = float64(memStat.HeapInuse)
+	stats["HeapObjects"] = float64(memStat.HeapObjects)
+	stats["HeapReleased"] = float64(memStat.HeapReleased)
+	stats["HeapSys"] = float64(memStat.HeapSys)
+	stats["LastGC"] = float64(memStat.LastGC)
+	stats["Lookups"] = float64(memStat.Lookups)
+	stats["MCacheInuse"] = float64(memStat.MCacheInuse)
+	stats["MCacheSys"] = float64(memStat.MCacheSys)
+	stats["MSpanInuse"] = float64(memStat.MSpanInuse)
+	stats["MSpanSys"] = float64(memStat.MSpanSys)
+	stats["Mallocs"] = float64(memStat.Mallocs)
+	stats["NextGC"] = float64(memStat.NextGC)
+	stats["NumForcedGC"] = float64(memStat.NumForcedGC)
+	stats["NumGC"] = float64(memStat.NumGC)
+	stats["OtherSys"] = float64(memStat.OtherSys)
+	stats["PauseTotalNs"] = float64(memStat.PauseTotalNs)
+	stats["StackInuse"] = float64(memStat.StackInuse)
+	stats["StackSys"] = float64(memStat.StackSys)
+	stats["Sys"] = float64(memStat.Sys)
+	stats["TotalAlloc"] = float64(memStat.TotalAlloc)
+}
+
+func sendMetrics(client *http.Client, stats stats) {
+
+	for key, val := range stats {
+		fmt.Println(key, val)
+		sendGaugeMetric(client, key, val)
+	}
+
+	sendCounterMetric(client, "PollCount", pollCount)
+}
+
+func sendGaugeMetric(client *http.Client, metricName string, metricValue float64) {
+	url := fmt.Sprintf("http://localhost:8080/update/gauge/%s/%f/", metricName, metricValue)
+
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	req.Header.Set("Content-type", "text/plain; charset=utf-8")
+
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	defer response.Body.Close()
+}
+
+func sendCounterMetric(client *http.Client, metricName string, metricValue int) {
+	url := fmt.Sprintf("http://localhost:8080/update/counter/%s/%d/", metricName, metricValue)
+
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	req.Header.Set("Content-type", "text/plain; charset=utf-8")
+
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	defer response.Body.Close()
+}
