316400f918eff71cd8abb042b2723a1995819f4a
Maksim Pozharskiy <pozharskii@bifit.com>
2025-01-04 10:52:25 +0300

    feat: add os envs

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strconv"
 )
 
 var flagRunAddr string
@@ -13,4 +15,20 @@ func parseFlags() {
 	flag.IntVar(&flagPollInterval, "p", 2, "how often to get metrics from runtime, seconds")
 	flag.IntVar(&flagReportInterval, "r", 10, "how often to send metrics to server, seconds")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
+
+	if envReportInterval := os.Getenv("REPORT_INTERVAL"); envReportInterval != "" {
+		if envReportInterval, err := strconv.Atoi(envReportInterval); err == nil {
+			flagReportInterval = envReportInterval
+		}
+	}
+
+	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
+		if envPollInterval, err := strconv.Atoi(envPollInterval); err == nil {
+			flagReportInterval = envPollInterval
+		}
+	}
 }
