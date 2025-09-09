c7fe0c75c747d558e26664276e1560db074fda41
Maksim Pozharskiy <max@zenbook.localdomain>
2025-09-09 18:58:27 +0300

    feat: implement RSA encryption between agent and server

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,12 +17,14 @@ type Config struct {
 	ReportInterval  int    // Interval for reporting metrics to the server, in seconds
 	HashKey         string // Secret key for signing metric payloads
 	RateLimit       int    // Number of parallel workers for sending metrics
+	CryptoKey       string // Path to the cryptographic key file
 }
 
 // GetConfig parses configuration from command-line flags and environment variables.
 func GetConfig() *Config {
 	cfg := &Config{}
 
+	flag.StringVar(&cfg.CryptoKey, "crypto-key", "", "path to the public/private key file")
 	flag.StringVar(&cfg.RunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.IntVar(&cfg.PollInterval, "p", 2, "how often to get metrics from runtime, seconds")
 	flag.IntVar(&cfg.ReportInterval, "r", 10, "how often to send metrics to server, seconds")
@@ -54,6 +56,10 @@ func GetConfig() *Config {
 		if envRateLimit, err := strconv.Atoi(envRateLimit); err == nil {
 			cfg.RateLimit = envRateLimit
 		}
+	}
+
+	if envCryptoKey := os.Getenv("CRYPTO_KEY"); envCryptoKey != "" {
+		cfg.CryptoKey = envCryptoKey
 	}
 
 	return cfg
